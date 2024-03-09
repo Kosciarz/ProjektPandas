1d3a764b41a163b8d570a682b093201ddbbfce4b
Bartosz <bartoszbachul1@gmail.com>
2024-03-09 19:46:10 +0100

    test

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import pandas as pd
 
 
+class Program:
+    file = None
+
+    @staticmethod
+    def set_file(file_path: str):
+        df = pd.read_csv(file_path)
+        Program.file = df
+
+    @staticmethod
+    def show_file():
+        print(Program.file)
+
+    @staticmethod
+    def show_columns():
+        column_names = Program.file.columns.tolist()
+        column_names.pop(0)
+        print("Nazwy wszystkich kolumn: ")
+        for index, name in enumerate(column_names, start=1):
+            print(f"{index}. {name}")
+
+
 def main():
-    file = pd.read_csv("194bcff35001e7eb53a2a8b441e8b2c6\pokemon.csv")
-    print(file)
+    file_path: str = ".\pokemon.csv"
+    Program.set_file(file_path)
+    Program.show_columns()
+    # Program.show_file()
+
     return 0
 
 
