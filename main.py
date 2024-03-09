22183364bc6ee4f6a14fe621a4ff4aa366232aea
Bartosz <bartoszbachul1@gmail.com>
2024-03-09 18:45:26 +0100

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def main():
+    file = pd.read_csv("194bcff35001e7eb53a2a8b441e8b2c6\pokemon.csv")
+    print(file)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
